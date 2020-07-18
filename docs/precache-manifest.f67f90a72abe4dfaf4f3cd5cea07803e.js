0afda807dcc58f6ae3ee6f3f89a339d61d668642
George <gathva@gmail.com>
2020-07-18 18:27:50 -0400

    docs agregado

---
+++ docs/precache-manifest.f67f90a72abe4dfaf4f3cd5cea07803e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26696879300d6db6aad885a675d82456",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c05e0d67753cfd9e684",
+    "url": "/static/css/main.3bdb3446.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c05e0d67753cfd9e684",
+    "url": "/static/js/main.5893af67.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
